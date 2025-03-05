3c1615e7532614031ec220a832a186c98b1fdec1
LavkushSolanki <lavkushsolanki12@gmail.com>
2025-03-05 12:46:54 +0530

    updated the backend url

--- Har_Ghar_Khaana/admin/src/App.jsx
+++ Har_Ghar_Khaana/admin/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
-import Sidebar from "./components/sidebar/Sidebar";
+import Sidebar from "./components/Sidebar/Sidebar";
 import Add from "./pages/Add/Add";
 import List from "./pages/List/List";
 import Orders from "./pages/Orders/Orders";
